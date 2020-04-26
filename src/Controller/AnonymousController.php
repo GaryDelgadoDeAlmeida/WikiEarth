33f9d5e9df076e6e9af74d75a82381d86f7019a5
Garry Almeida <48718452+GaryDelgadoDeAlmeida@users.noreply.github.com>
2020-04-26 16:23:05 +0200

    Start menu structure

--- src/Controller/AnonymousController.php
+++ src/Controller/AnonymousController.php
@@ -10,10 +10,16 @@ class AnonymousController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index()
+    public function home()
     {
-        return $this->render('anonymous/home/index.html.twig', [
-            'controller_name' => 'AnonymousController',
-        ]);
+        return $this->render('anonymous/home/index.html.twig');
+    }
+
+    /**
+     * @Route("/about", name="about")
+     */
+    public function about()
+    {
+        return $this->render('anonymous/about/index.html.twig');
     }
 }
